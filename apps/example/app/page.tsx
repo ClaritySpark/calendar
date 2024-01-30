a537bc84d52d3170b2d0e1ffb344a5e5b231e724
pmhxxhsj <minho7952@naver.com>
2024-01-30 23:51:45 +0900

    fix: remove test var

--- apps/example/app/page.tsx
+++ apps/example/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home(): JSX.Element {
-  const a: any = 10;
-
   return <main>Example</main>;
 }
--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -3,6 +3,5 @@ import React, { ButtonHTMLAttributes, PropsWithChildren } from "react";
 type Props = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
 
 export function Button({ children, ...rest }: Props) {
-  const b: any = 10;
   return <button {...rest}>{children}</button>;
 }
